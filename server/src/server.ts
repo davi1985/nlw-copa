651f977d80ca5e136b58f0c0a9799ba7e85fd5f3
Davi Silva <davisilvaphoto@gmail.com>
2022-11-03 11:33:46 -0300

    feat: create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
-import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import ShortUniqueId from "short-unique-id";
+import { z } from "zod";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -18,6 +20,41 @@ async function bootstrap() {
 
     return { count };
   });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  fastify.post(
+    "/pools",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const createPoolBody = z.object({
+        title: z.string(),
+      });
+
+      const { title } = createPoolBody.parse(request.body);
+
+      const generate = new ShortUniqueId({ length: 6 });
+      const code = String(generate()).toUpperCase();
+
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+
+      return reply.status(201).send({ code });
+    }
+  );
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
